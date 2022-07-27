0b4bc6474b6739a7f44cde28d958fa259e1f48b7
Diego Baeza <diegob12@live.com>
2022-07-27 16:21:49 -0500

    initial commit, added test data for db

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+-- Create dabase called 'auth_shelf'
+CREATE DATABASE "auth_shelf";
 
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
@@ -15,3 +17,40 @@ CREATE TABLE "item" (
     "image_url" VARCHAR (2083),
     "user_id" INT REFERENCES "user"
 );
+
+
+-- Add dummy data by registering for a user, examples below
+
+-- INSERT INTO "user" (
+--     "username", "password"
+-- )
+-- VALUES (
+--     'Timmy Turner',
+--     'OddParents'
+-- ), (
+--     'Magic Mike',
+--     'Muscles'    
+-- );
+
+
+-- dummy data for items
+
+INSERT INTO "item" (
+    "description",
+    "image_url",
+    "user_id"
+)
+VALUES (
+    'Mac n Cheese',
+    'https://imgur.com/gallery/RK8Vi',
+    '1'
+), (
+    'Big Mac',
+    'https://imgur.com/gallery/CnJ0r',
+    '1'
+), (
+    'Maple Pie',
+    'https://i.imgur.com/XKQTiT7.jpg',
+    '2'
+);
+
